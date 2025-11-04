5da143e10505b98d3d706a553bc206875a6ac488
leejinhyuk0212 <myjinhyuk@tukorea.ac.kr>
2025-11-04 19:49:32 +0900

    새 움직임 애니메이션 구현
    새의 이동속도는 PIXEL_PER_METER 1m당 33.3 픽셀을 움직인다. 그래서 속도(velocity)가 10이므로 초당 333픽셀 이동을 함. 날개짓 속도는 초당 10프레임씩 바뀌니깐 초당 2번날개짓을함

--- bird.py
+++ bird.py
@@ -4,20 +4,26 @@ import game_framework
 
 PIXEL_PER_METER = (1.0/0.03)
 
+TIME_PER_ACTION = 0.5
+ACTION_PER_TIME = 1.0/ TIME_PER_ACTION
+FRAME_PER_ACTION = 5
+FRAME_PER_SECOND = FRAME_PER_ACTION * ACTION_PER_TIME
+
 class Bird:
     image = None
 
-    def __init__(self, x = 400, y = 300, velocity = 10):
+    def __init__(self, x = 400, y = 300, velocity = 0):
         if Bird.image == None:
             Bird.image = load_image('bird_animation.png')
         self.x, self.y = x, y
         self.velocity = velocity
+        self.frame = 0.0
 
     def draw(self):
-        self.image.draw(self.x, self.y)
+        self.image.clip_draw(int(self.frame) * 184, 340, 184, 169, self.x, self.y)
 
     def update(self):
-
+        self.frame = (self.frame + FRAME_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 5
         self.x += (self.velocity * game_framework.frame_time * PIXEL_PER_METER)
 
         if self.x < 0 or self.x >600:
